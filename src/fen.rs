1ea3d39133ca67804e57d903b828a0f37f5b0d99
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2025-07-12 15:10:55 +0200

    beginning of fen parsing

--- src/fen.rs
+++ src/fen.rs
@@ -1,1 +1,13 @@
-pub struct Fen {}
+pub struct Fen {
+    pub file_path: String,
+}
+
+impl Fen {
+    pub fn process(&self) {
+        let fields: Vec<&str> = self
+            .file_path
+            .split(|c| c == '/' || c == ' ')
+            .to_owned()
+            .collect();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod board;
+mod fen;
 mod piece;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File, path::Path};
 
 use board::*;
+use fen::*;
 use piece::*;
 
 fn main() {
-    let mut init: HashMap<Square, Option<Piece>> = HashMap::new();
+    //replace with command line arg in the future;
+    let file_string = Path::new("../savefiles/init.fen").to_str().unwrap();
+    let fen = Fen {
+        file_path: String::from(file_string),
+    };
+
+    let init: HashMap<Square, Option<Piece>> = HashMap::new();
     // notation
     let board = Board { board_state: init };
     board.pp();
